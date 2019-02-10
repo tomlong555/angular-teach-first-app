ea061074c0c302a7ed4a56582073970eccb0c7bb
tom <gogogo1120@hotmail.com>
2019-02-10 14:34:35 +0700

    'gg'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,23 @@
 import { Component } from '@angular/core';
-
+import { Card } from './card';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  cards: Card[] = [];
+  get sorted():Card[]{
+    console.log('accsecc');
+    return this.cards.sort((a,b) => b.votes - a.votes);
+  }
+
+  onclick(firstName: HTMLInputElement,lastName: HTMLInputElement) {
+    const card = new Card(firstName.value,lastName.value,0);
+    this.cards.push(card);
+    console.log(this.cards);
+
+    firstName.value = '';
+    lastName.value = '';
+  }
 }
---
+++ src/app/card.ts
@@ -0,0 +1,14 @@
+export class Card {
+    firstName:string;
+    lastName:string;
+    votes:number;
+    constructor(firstName:string,lastName:string,votes:number){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.votes = votes;
+    }
+
+    get name():string{
+        return this.firstName +' '+this.lastName;
+    }
+}
